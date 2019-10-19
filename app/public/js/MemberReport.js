078e60175d7a0bbccc6a2210d14d8057c39758a1
Andrew <ajrunyon@iu.edu>
2019-10-19 17:06:40 -0400

    Other shells for js files made
    other js files made, need to be changed for the needs of their corresponding html pages.

---
+++ app/public/js/MemberReport.js
@@ -0,0 +1,53 @@
+var MemberReportapp = new Vue({
+  el: '#MemberReportapp',
+  data: {
+    Certifications: [],
+    recordCertifications: {},
+  },
+  methods: {
+    fetchCertifications() {
+  fetch('api/certificationrecords/index.php')
+  .then(response => response.json())
+  .then(json => { generalcertificationsapp.Certifications = json })
+},
+handleSubmit(event) {
+  // fetch(url, {
+  //   method: 'post',
+  //   data: this.recordPatient
+  // })
+  // .then( ... )
+  fetch('api/certificationrecords/post.php', {
+    method:'POST',
+    body: JSON.stringify(this.recordCertifications),
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    }
+  })
+  .then( response => response.json() )
+  .then( json => { waitingApp.Certifications = json})
+//.catch( err => {
+  //console.error('WORK GENCERT ERROR:');
+  //console.error(err);
+//})
+  //this.Certifications.push( this.recordCertifications );
+  this.handleReset();
+},
+handleReset() {
+  this.recordCertifications = {
+    certID: '',
+    agency: '',
+    certname: '',
+    expires: ''
+  }
+    },
+
+
+//    handleRowClick(certifications) {
+//  generalcertificationsApp.Certifications = certifications;
+//}
+  },
+  created() {
+  this.handleReset();
+  this.fetchCertifications();
+}
+});
